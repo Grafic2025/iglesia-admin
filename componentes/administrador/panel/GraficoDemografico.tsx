b6bd3472fc5c3157af66a15edbd6e2921b26685f
Emilio Pujalka <epujalka@directo.com.ar>
2026-03-09 14:55:44 -0300

    arreglos 1

--- componentes/administrador/panel/GraficoDemografico.tsx
+++ componentes/administrador/panel/GraficoDemografico.tsx
@@ -15,7 +15,7 @@ const GraficoDemografico: React.FC<GraficoDemograficoProps> = ({ data, COLORS })
             </h3>
             <div className="h-[250px]">
                 <ResponsiveContainer width="100%" height={250}>
-                    <ReLineBarChart data={data} diseno="vertical">
+                    <ReLineBarChart data={data} layout="vertical">
                         <CartesianGrid strokeDasharray="3 3" stroke="#333" horizontal={false} />
                         <XAxis type="number" stroke="#888" fontSize={10} />
                         <YAxis dataKey="nombre" type="category" stroke="#888" fontSize={10} width={120} />
